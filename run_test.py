3f7be13a8f4863cdea341880f9c3e5eb798b140c
hanzholahs <hanzholahs@gmail.com>
2023-02-12 10:38:50 +0700

    Create script for running all test cases

---
+++ run_test.py
@@ -0,0 +1,8 @@
+import unittest
+from test.test_genome import *
+from test.test_phenotype import *
+from test.test_creature import *
+from test.test_population import *
+from test.test_evolution import *
+
+unittest.main()
