29d073942949ed142c31e951045383ef018921de
hanzholahs <hanzholahs@gmail.com>
2023-02-18 21:24:25 +0700

    Add execution test

--- run_test.py
+++ run_test.py
@@ -5,5 +5,6 @@ from test.test_creature import *
 from test.test_population import *
 from test.test_evolution import *
 from test.test_simulator import *
+from test.test_execution import *
 
 unittest.main()
