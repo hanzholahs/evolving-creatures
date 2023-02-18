6616a6c3ed4d556d35388ed71a5c7fa03836b8db
hanzholahs <hanzholahs@gmail.com>
2023-02-18 21:23:24 +0700

    Define method to get new generation of pop
    - set assertion of parameters
    - add capability to limit elite creatures
    - add capability to generate new random creatures

--- creatures/population.py
+++ creatures/population.py
@@ -1,4 +1,5 @@
-from creatures import creature
+import numpy as np
+from creatures import creature, evolution
 
 
 class Population:
@@ -29,6 +30,63 @@ class Population:
     def add_creatures(self, creatures:list[creature.Creature]):
         self.creatures.extend(creatures)
         self.population_size = len(self.creatures)
+
+    def new_generation(self,
+                       num_of_elites:int,
+                       num_of_random:int,
+                       min_length:int,
+                       max_length:int,
+                       max_growth_rt:float,
+                       mutation_freq:float,
+                       mutation_amnt:float,
+                       dist_limit_rt:float = 1.025):
+        assert num_of_elites < self.population_size
+        assert num_of_random < self.population_size
+        assert num_of_elites + num_of_random < self.population_size
+        assert 0 < min_length and min_length <= max_length
+        assert 0 <= max_growth_rt
+        assert 0 <= mutation_freq and mutation_freq <= 1
+        assert 0 <= mutation_amnt and mutation_amnt <= 1
+
+        # eliminate cheating creatures manually by incremental fit increase
+        for cr in self.creatures:
+            if cr.get_distance() > self.max_dist * dist_limit_rt:
+                cr.update_position((0, 0, 0))
+
+        # update max dist
+        dists = np.array([cr.get_distance() for cr in self.creatures])
+        self.n_invalid = np.sum(dists == 0.0)
+        self.min_dist = np.min(dists)
+        self.max_dist = np.max(dists)
+        self.avg_dist = np.mean(dists)
+
+        # genetic algo
+        fits = evolution.Selection.eval_fitness(self.creatures)
+        fittest_indices = np.array(fits).argsort()[-1:-(num_of_elites+1):-1]
+
+        new_creatures = []
+        for index in fittest_indices:
+            fittest_cr = self.creatures[index]
+            new_creatures.append(fittest_cr)
+        for _ in range(self.population_size - num_of_elites - num_of_random):
+            fits = evolution.Selection.eval_fitness(self.creatures)
+            p1, p2 = evolution.Selection.select_parents(self.creatures, fits)
+            child_cr = creature.Creature(1)
+            child_cr.update_dna(evolution.Mating.mate(
+                p1.dna,
+                p2.dna,
+                min_length,
+                max_length,
+                max_growth_rt,
+                mutation_freq,
+                mutation_amnt
+            ))
+            new_creatures.append(child_cr)
+        for _ in range(num_of_random):
+            random_cr = creature.Creature(self.default_gene_count)
+            new_creatures.append(random_cr)
+
+        self.reset_population(new_creatures)
 
 
 
--- test/test_population.py
+++ test/test_population.py
@@ -1,8 +1,13 @@
 import unittest
-from creatures import population, creature
+import numpy as np
+from creatures import population, creature, genome
 
 class PopulationTest(unittest.TestCase):
     def testPopulationCreatures(self):
+        self.assertIsNotNone(population.Population)
+        self.assertIsNotNone(population.Population.add_creature)
+        self.assertIsNotNone(population.Population.add_creatures)
+
         pop = population.Population(10, 3)
         self.assertIsNotNone(pop)
         self.assertEqual(len(pop.creatures), 10)
@@ -10,7 +15,8 @@ class PopulationTest(unittest.TestCase):
         pop.reset_population([creature.Creature(10), creature.Creature(10)])
         self.assertEqual(len(pop.creatures), 2)
         with self.assertRaises(AssertionError):
-            pop.reset_population(creature.Creature(10)) # requires a list of crs
+            # `reset_population` method requires a list of crs
+            pop.reset_population(creature.Creature(10)) 
         
         pop.add_creature(creature.Creature(20))
         self.assertEqual(len(pop.creatures), 3)
@@ -18,6 +24,95 @@ class PopulationTest(unittest.TestCase):
         pop.add_creatures([creature.Creature(10), creature.Creature(10)])
         self.assertEqual(len(pop.creatures), 5)
 
-        pop.population_size = 10
-        pop.reset_population()
-        self.assertEqual(len(pop.creatures), 10)
+    def testResetPopulation(self):
+        self.assertIsNotNone(population.Population.reset_population)
+
+        pop = population.Population(5, 3)
+        dna1 = pop.creatures[0].dna
+
+        for pop_size in range(1, 20):
+            pop.population_size = pop_size
+            pop.reset_population()
+            dna2 = pop.creatures[0].dna
+            self.assertEqual(len(pop.creatures), pop_size)
+            self.assertTrue((dna1 != dna2).all())
+
+    def testNewGeneration(self):
+        self.assertIsNotNone(population.Population.new_generation)
+
+        pop_size = 50
+        num_of_elites = 5
+        num_of_random = 5
+        min_length = 2
+        max_length = 10
+        max_growth_rt = 1.5
+        mutation_freq = 0.15
+        mutation_amnt = 0.15
+
+        pop = population.Population(pop_size, 3)
+
+        for cr in pop.creatures:
+            cr.last_position = (np.random.normal(),
+                                np.random.normal(),
+                                np.random.normal())
+            
+        for _ in range(15):
+            pop.new_generation(
+                num_of_elites = num_of_elites,
+                num_of_random = num_of_random,
+                min_length = min_length,
+                max_length = max_length,
+                max_growth_rt = max_growth_rt,
+                mutation_freq = mutation_freq,
+                mutation_amnt = mutation_amnt,
+            )
+            self.assertEqual(len(pop.creatures), pop_size)
+            for cr in pop.creatures:
+                self.assertEqual(cr.dna.shape[1], len(genome.Genome.get_spec()))
+            
+        with self.assertRaises(AssertionError):
+            pop.new_generation(
+                num_of_elites = num_of_elites,
+                num_of_random = num_of_random,
+                min_length = 100,
+                max_length = max_length,
+                max_growth_rt = max_growth_rt,
+                mutation_freq = mutation_freq,
+                mutation_amnt = mutation_amnt,
+            )
+                        
+        with self.assertRaises(AssertionError):
+            pop.new_generation(
+                num_of_elites = num_of_elites,
+                num_of_random = num_of_random,
+                min_length = min_length,
+                max_length = max_length,
+                max_growth_rt = -1,
+                mutation_freq = mutation_freq,
+                mutation_amnt = mutation_amnt,
+            )
+                        
+        with self.assertRaises(AssertionError):
+            pop.new_generation(
+                num_of_elites = num_of_elites,
+                num_of_random = num_of_random,
+                min_length = min_length,
+                max_length = max_length,
+                max_growth_rt = max_growth_rt,
+                mutation_freq = 2,
+                mutation_amnt = mutation_amnt,
+            )
+                        
+        with self.assertRaises(AssertionError):
+            pop.new_generation(
+                num_of_elites = 100,
+                num_of_random = num_of_random,
+                min_length = min_length,
+                max_length = max_length,
+                max_growth_rt = max_growth_rt,
+                mutation_freq = mutation_freq,
+                mutation_amnt = mutation_amnt,
+            )
+        
+        # fits = evolution.Selection.eval_fitness(pop.creatures)
+        pass
