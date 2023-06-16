0e9129afc8b742a8b78011bc4126599b2baa724d
hanzholahs <hanzholahs@gmail.com>
2023-06-16 20:40:58 +0700

    Ensure identical creatures after write/load to csv

--- test/test_population.py
+++ test/test_population.py
@@ -1,8 +1,11 @@
+import os
+import shutil
 import unittest
 import numpy as np
-from creatures import population, creature, genome
+from creatures import population, evolution, genome, creature
 
 class PopulationTest(unittest.TestCase):
+
     def testPopulationCreatures(self):
         self.assertIsNotNone(population.Population)
         self.assertIsNotNone(population.Population.add_creature)
@@ -37,6 +40,41 @@ class PopulationTest(unittest.TestCase):
             self.assertEqual(len(pop.creatures), pop_size)
             self.assertTrue((dna1 != dna2).all())
 
+    def testReadWritePopulation(self):
+        num_cr = 10
+        base_dir = ".tmp/test_population"
+        
+        pop1 = population.Population(num_cr, 5)
+        pop1.to_csvs(f"{base_dir}/data", "dna")
+        pop2 = population.Population(1)
+        pop2.from_csvs(f"{base_dir}/data", "dna")
+        self.assertEqual(len(pop1.creatures), len(pop2.creatures))
+        
+        for i in range(num_cr):
+            # creatures have identical dna
+            dna1 = pop1.creatures[i].dna
+            dna2 = pop2.creatures[i].dna
+            self.assertTrue(np.all(dna1 == dna2))
+            
+            # creatures have identical xml string
+            xml1 = pop1.creatures[i].get_xml().toprettyxml()
+            xml2 = pop2.creatures[i].get_xml().toprettyxml()
+            self.assertEqual(xml1, xml2)
+            
+            # creatures have identical motors
+            motors1 = pop1.creatures[i].get_motors()
+            motors2 = pop2.creatures[i].get_motors()
+            self.assertEqual(len(motors1), len(motors2))
+            for j in range(len(motors1)):
+                self.assertEqual(motors2[j].motor_type, motors1[j].motor_type)
+                self.assertEqual(motors2[j].step_size, motors1[j].step_size)
+                self.assertEqual(motors2[j].amplitude, motors1[j].amplitude)
+                self.assertEqual(motors2[j].param1, motors1[j].param1)
+                self.assertEqual(motors2[j].param2, motors1[j].param2)
+                self.assertEqual(motors2[j].phase, motors1[j].phase)
+        
+        shutil.rmtree(base_dir)
+
     def testNewGeneration(self):
         self.assertIsNotNone(population.Population.new_generation)
 
@@ -48,13 +86,11 @@ class PopulationTest(unittest.TestCase):
         max_growth_rt = 1.5
         mutation_freq = 0.15
         mutation_amnt = 0.15
-
+        rand = np.random.normal
+    
         pop = population.Population(pop_size, 3)
-
         for cr in pop.creatures:
-            cr.last_position = (np.random.normal(),
-                                np.random.normal(),
-                                np.random.normal())
+            cr.last_position = (rand(), rand(), rand())
             
         for _ in range(15):
             pop.new_generation(
@@ -113,6 +149,3 @@ class PopulationTest(unittest.TestCase):
                 mutation_freq = mutation_freq,
                 mutation_amnt = mutation_amnt,
             )
-        
-        # fits = evolution.Selection.eval_fitness(pop.creatures)
-        pass
