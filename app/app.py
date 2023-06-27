42a1365770c3f7d785a6335f0ce4d421dcd8d852
hanzholahs <hanzholahs@gmail.com>
2023-06-27 22:12:45 +0700

    Correct parameters, add save & load app methods

--- app/app.py
+++ app/app.py
@@ -1,30 +1,33 @@
+import os
 from app import simulator
 from creatures import population
 
 class MainApp:
-    def __init__(
-        self, 
-        multiprocess:bool = True, 
-        pool_size:int = 5,
-        max_frame:int = 1200,
-        population_size:int = 5,
-        num_of_generation:int = 10,
-        default_gene_count:int = 5,
-        num_of_elites:int = 1,
-        num_of_random:int = 1,
-        min_length:int = 2,
-        max_length:int = 5,
-        max_growth_rt:float = 1.1,
-        mutation_freq:float = 0.1,
-        mutation_amnt:float = 0.1,
-        dist_limit_rt:float = 1.025
-    ) -> None:
+    def __init__(self,
+                 base_dir:str = ".sim",
+                 multiprocess:bool = True,
+                 pool_size:int = 5,
+                 max_frame:int = 1200,
+                 population_size:int = 5,
+                 current_generation:int = 0,
+                 num_of_generation:int = 10,
+                 default_gene_count:int = 5,
+                 num_of_elites:int = 1,
+                 num_of_random:int = 1,
+                 min_length:int = 2,
+                 max_length:int = 5,
+                 max_growth_rt:float = 1.1,
+                 mutation_freq:float = 0.1,
+                 mutation_amnt:float = 0.1,
+                 dist_limit_rt:float = 1.025) -> None:
         
         # Set app specification
+        self.base_dir = base_dir
         self.multiprocess = multiprocess
         self.pool_size = pool_size
         self.max_frame = max_frame
         self.population_size = population_size
+        self.current_generation = current_generation
         self.num_of_generation = num_of_generation
         self.default_gene_count = default_gene_count
         self.num_of_elites = num_of_elites
@@ -40,16 +43,22 @@ class MainApp:
 
         # instantiate pop and sim
         self.reset_population()
+        self.save_population()
         self.build_simulator()
         
     def build_simulator(self, 
-                        multiprocess: bool = None, 
-                        pool_size: int = None) -> None:
+                        multiprocess:bool = None, 
+                        pool_size:int = None) -> None:
+        
         # Change sim specification
         if multiprocess is not None:
             self.multiprocess = multiprocess
         if pool_size is not None:
             self.pool_size = pool_size
+        if ((self.multiprocess == False and self.pool_size > 1) or
+            (self.multiprocess == True and self.pool_size < 2)):         
+            raise Exception(f"Multiprocess cannot {self.multiprocess} " +
+                            f"while pool size is {self.pool_size}")
 
         # delete simulator object(s)
         if self.sim is not None:
@@ -65,8 +74,9 @@ class MainApp:
             self.sim = simulator.Simulator()
             
     def reset_population(self, 
-                         population_size: int = None, 
+                         population_size:int = None, 
                          default_gene_count:int = None) -> None:
+        
         # Change pop specification
         if population_size is not None:
             self.population_size = population_size
@@ -83,16 +93,22 @@ class MainApp:
             self.pop.reset_population()
         
     def run(self,
-            num_of_generation: int = None,
-            num_of_elites: int = None,
-            num_of_random: int = None,
-            min_length: int = None,
-            max_length: int = None,
-            max_growth_rt: float = None,
-            mutation_freq: float = None,
-            mutation_amnt: float = None,
-            dist_limit_rt: float = None) -> None:
+            base_dir:str = None,
+            save_population:bool = False,
+            save_each:int = None,
+            num_of_generation:int = None,
+            num_of_elites:int = None,
+            num_of_random:int = None,
+            min_length:int = None,
+            max_length:int = None,
+            max_growth_rt:float = None,
+            mutation_freq:float = None,
+            mutation_amnt:float = None,
+            dist_limit_rt:float = None) -> None:
+        
         # change sim run specification
+        if base_dir is not None:
+            self.base_dir = base_dir
         if num_of_generation is not None:
             self.num_of_generation = num_of_generation
         if num_of_elites is not None:
@@ -113,7 +129,7 @@ class MainApp:
             self.dist_limit_rt = dist_limit_rt
         
         # run simulation for some generations
-        for _ in range(self.num_of_generation - 1):
+        for i in range(self.num_of_generation - 1):
             self.sim.eval_population(self.pop, self.max_frame)
             self.pop.new_generation(
                 self.num_of_elites,
@@ -125,5 +141,36 @@ class MainApp:
                 self.mutation_amnt,
                 self.dist_limit_rt
             )
+            self.current_generation += 1
+            if save_each is not None and i % save_each == 0:
+                self.save_population()
+                
         self.sim.eval_population(self.pop)
+        self.current_generation += 1
         
+        # save csvs after simulation
+        if save_population:
+            self.save_population()
+        
+    def save_population(self, base_dir = None) -> None:
+        if base_dir is not None:
+            self.base_dir = base_dir
+        
+        save_path = os.path.join(self.base_dir, "pop", str(self.current_generation))
+        
+        if not os.path.exists(save_path):
+            os.makedirs(save_path, exist_ok = True)
+        
+        self.pop.to_csvs(base_folder = save_path, identifier = "cr")
+    
+    def load_population(self, 
+                        base_dir = None, 
+                        current_generation:int = None) -> None:
+        if base_dir is not None:
+            self.base_dir = base_dir
+        if current_generation is None:
+            self.current_generation = max([int(d) for d in os.listdir(self.base_dir + "/pop")])
+            
+        load_path = os.path.join(self.base_dir, "pop", str(self.current_generation))
+        
+        self.pop.from_csvs(base_folder = load_path, identifier = "cr")
--- test/test_mainapp.py
+++ test/test_mainapp.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 import numpy as np
 from app import app, simulator
@@ -6,11 +8,14 @@ from creatures import population
 class AppTest(unittest.TestCase):
     def testClassExists(self):
         self.assertIsNotNone(app.MainApp)
+        
+        
+    def testAppCanRun(self):
         self.assertIsNotNone(app.MainApp.reset_population)
         self.assertIsNotNone(app.MainApp.build_simulator)
         self.assertIsNotNone(app.MainApp.run)
         
-        main = app.MainApp()
+        main = app.MainApp(".tmp/simulation-test-consistency")
         self.assertIsNotNone(main)
         self.assertIsNotNone(main.sim)
         self.assertIsNotNone(main.pop)
@@ -21,6 +26,7 @@ class AppTest(unittest.TestCase):
         
         sim_type_1 = type(main.sim)
         main.multiprocess = False
+        main.pool_size = 1
         main.build_simulator()
         sim_type_2 = type(main.sim)
         
@@ -33,3 +39,70 @@ class AppTest(unittest.TestCase):
         self.assertEqual(sim_type_2, simulator.Simulator)
         self.assertGreater(np.sum(dists_1), 0)
         self.assertEqual(np.sum(dists_2), 0)
+    
+    
+    def testMultiprocessAndPoolSizeConsistency(self):
+        main = app.MainApp(".tmp/simulation-test-consistency")
+        
+        with self.assertRaises(Exception) as context:
+            main.build_simulator(multiprocess = True, pool_size = 1)
+        with self.assertRaises(Exception) as context:
+            main.build_simulator(multiprocess = True, pool_size = 1)
+        
+        
+    def testAppSavePopulation(self):
+        self.assertIsNotNone(app.MainApp.save_population)
+        
+        base_dir  = ".tmp/simulation-test-save"
+        pop_size  = 10
+        num_gens  = 5
+        save_each = 1
+        
+        if os.path.exists(base_dir): shutil.rmtree(base_dir)
+        
+        main = app.MainApp(base_dir = base_dir, population_size = pop_size)
+        self.assertTrue(os.path.exists(base_dir))
+        self.assertEqual(len(os.listdir(base_dir + "/pop/0")), pop_size)
+        
+        main.run(save_population = True,
+                 save_each = save_each,
+                 base_dir = base_dir, 
+                 num_of_generation = num_gens)
+        self.assertEqual(len(os.listdir(base_dir + "/pop")), num_gens + 1)
+        
+        for i in range(num_gens + 1):
+            self.assertEqual(len(os.listdir(f"{base_dir}/pop/{i}")), pop_size)
+        
+        
+    def testAppLoadPopulation(self):
+        self.assertIsNotNone(app.MainApp.load_population)
+        
+        old_creatures = []
+        base_dir  = ".tmp/simulation-test-load"
+        pop_size  = 5
+        num_gens  = 3
+        
+        if os.path.exists(base_dir): shutil.rmtree(base_dir)
+        
+        main = app.MainApp(base_dir = base_dir, population_size = pop_size)
+        main.run(save_population = True, num_of_generation = num_gens)
+        main.build_simulator(False, 1)
+        for cr in main.pop.creatures:
+            old_creatures.append(cr)
+            del cr
+        del main.sim
+        del main.pop
+        del main
+        
+        new = app.MainApp(base_dir = base_dir, population_size = pop_size)
+        new.load_population()
+        for i, cr in enumerate(new.pop.creatures):
+            self.assertTrue((cr.dna == old_creatures[i].dna).all())
+        
+        
+    def testAppDeterministicRun(self):
+        pass
+    
+    
+    def testAppGenerateReports(self):
+        pass
