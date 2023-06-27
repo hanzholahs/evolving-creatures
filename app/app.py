a9e907dd6419953bb4dc4b46534b29af4ae020c0
hanzholahs <hanzholahs@gmail.com>
2023-06-27 20:32:02 +0700

    parameterised MainApp simulation functions

--- app/app.py
+++ app/app.py
@@ -7,8 +7,8 @@ class MainApp:
         multiprocess:bool = True, 
         pool_size:int = 5,
         max_frame:int = 1200,
-        num_of_generation:int = 10,
         population_size:int = 5,
+        num_of_generation:int = 10,
         default_gene_count:int = 5,
         num_of_elites:int = 1,
         num_of_random:int = 1,
@@ -20,12 +20,13 @@ class MainApp:
         dist_limit_rt:float = 1.025
     ) -> None:
         
+        # Set app specification
         self.multiprocess = multiprocess
         self.pool_size = pool_size
         self.max_frame = max_frame
         self.population_size = population_size
-        self.default_gene_count = default_gene_count
         self.num_of_generation = num_of_generation
+        self.default_gene_count = default_gene_count
         self.num_of_elites = num_of_elites
         self.num_of_random = num_of_random
         self.min_length = min_length
@@ -37,22 +38,42 @@ class MainApp:
         self.pop = None
         self.sim = None
 
+        # instantiate pop and sim
         self.reset_population()
         self.build_simulator()
         
-    def build_simulator(self):
+    def build_simulator(self, 
+                        multiprocess: bool = None, 
+                        pool_size: int = None) -> None:
+        # Change sim specification
+        if multiprocess is not None:
+            self.multiprocess = multiprocess
+        if pool_size is not None:
+            self.pool_size = pool_size
+
+        # delete simulator object(s)
         if self.sim is not None:
             if type(self.sim) == simulator.MultiSimulator:
                 for sim in self.sim.sims:
                     del sim
             del self.sim
-        
+
+        # instantiate new simulator       
         if self.multiprocess:
             self.sim = simulator.MultiSimulator(self.pool_size)
         else:
             self.sim = simulator.Simulator()
             
-    def reset_population(self):
+    def reset_population(self, 
+                         population_size: int = None, 
+                         default_gene_count:int = None) -> None:
+        # Change pop specification
+        if population_size is not None:
+            self.population_size = population_size
+        if default_gene_count is not None:
+            self.default_gene_count = default_gene_count        
+
+        # Instatiate population
         if self.pop == None:
             self.pop = population.Population(
                 population_size = self.population_size,
@@ -61,7 +82,37 @@ class MainApp:
         else:
             self.pop.reset_population()
         
-    def run(self):
+    def run(self,
+            num_of_generation: int = None,
+            num_of_elites: int = None,
+            num_of_random: int = None,
+            min_length: int = None,
+            max_length: int = None,
+            max_growth_rt: float = None,
+            mutation_freq: float = None,
+            mutation_amnt: float = None,
+            dist_limit_rt: float = None) -> None:
+        # change sim run specification
+        if num_of_generation is not None:
+            self.num_of_generation = num_of_generation
+        if num_of_elites is not None:
+            self.num_of_elites = num_of_elites
+        if num_of_random is not None:
+            self.num_of_random = num_of_random
+        if min_length is not None:
+            self.min_length = min_length
+        if max_length is not None:
+            self.max_length = max_length
+        if max_growth_rt is not None:
+            self.max_growth_rt = max_growth_rt
+        if mutation_freq is not None:
+            self.mutation_freq = mutation_freq
+        if mutation_amnt is not None:
+            self.mutation_amnt = mutation_amnt
+        if dist_limit_rt is not None:
+            self.dist_limit_rt = dist_limit_rt
+        
+        # run simulation for some generations
         for _ in range(self.num_of_generation - 1):
             self.sim.eval_population(self.pop, self.max_frame)
             self.pop.new_generation(
@@ -75,3 +126,4 @@ class MainApp:
                 self.dist_limit_rt
             )
         self.sim.eval_population(self.pop)
+        
